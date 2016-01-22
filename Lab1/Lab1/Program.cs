1b0d414afad94179042f1d981ea297ab86a949ed
Ramazan Bilyalov <Ramazan Bilyalov>
2016-01-22 12:23:10 +0600

    1 zada4a

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n;
+            n = Console.ReadLine();
+            string[] prime = n.Split();
+            foreach(string x in prime)
+            {
+                int sum = 0;
+                for (int i=1; i < int.Parse(x); i++)
+                {
+                    if (int.Parse(x) % i == 0){
+                        sum++;
+                    }
+                
+                }
+                if (sum <=1)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+            Console.ReadKey();
+    }
+    }
+}
